e7fe97b568992678f9c143644dd8b02b09247c91
Matheus Wismeck <wismeck12@gmail.com>
2024-12-20 06:22:12 -0300

    algumas rotas rapidas

--- main.go
+++ main.go
@@ -9,15 +9,31 @@ import (
 func getEmployee(c echo.Context) error{
 	return c.String(http.StatusOK,"List of all employees")
 }
-
+func createEmployee(c echo.Context)error{
+	return c.String(http.StatusOK, "Create student")
+}
+func getEmployeeId(c echo.Context) error{
+	id := c.Param("id")
+	return c.String(http.StatusOK,"Get a employee")
+}
+func updateEmployee(c echo.Context) error{
+	return c.String(http.StatusOK,"Update a employee")
+}
+func deleteEmployee(c echo.Context) error{
+	return c.String(http.StatusOK,"Delete a employee")
+}
 
 func main() {
 	e := echo.New()
+	// START SERVER
+	e.Logger.Fatal(e.Start(":8080"))
 
 	//ROUTES
 	e.GET("/employee", getEmployee)
-	// START SERVER
-	e.Logger.Fatal(e.Start(":8080"))
+	e.POST("/employee", createEmployee)
+	e.GET("/employee/:id", getEmployeeId)
+	e.PUT("/employee/:id", updateEmployee)
+	e.DELETE("/employee/:id", deleteEmployee)
 
 	
 }
